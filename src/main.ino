0d275d3d66473b7ef08e1a003b104e87b0bb6d52
Jackonmelo <jacksommelo10@gmail.com>
2026-04-22 22:36:58 -0300

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -1,40 +1,59 @@
+#include <DHT.h>
+
 #define DHTPIN 15
 #define DHTTYPE DHT22
 
-#include <DHT.h>
 DHT dht(DHTPIN, DHTTYPE);
 
-#define rele 5
-#define botaoN 18
-#define botaoP 19
-#define botaoK 21
-#define ldr 34
+// Botões (NPK)
+#define BTN_N 32
+#define BTN_P 33
+#define BTN_K 25
+
+// LDR (pH simulado)
+#define LDR 34
+
+// Relé (bomba)
+#define RELE 26
 
 void setup() {
-  Serial.begin(9600);
-  dht.begin();
+  Serial.begin(115200);
 
-  pinMode(rele, OUTPUT);
-  pinMode(botaoN, INPUT_PULLUP);
-  pinMode(botaoP, INPUT_PULLUP);
-  pinMode(botaoK, INPUT_PULLUP);
+  pinMode(BTN_N, INPUT_PULLUP);
+  pinMode(BTN_P, INPUT_PULLUP);
+  pinMode(BTN_K, INPUT_PULLUP);
+
+  pinMode(RELE, OUTPUT);
+
+  dht.begin();
 }
 
 void loop() {
 
+  // Leitura sensores
+  int N = !digitalRead(BTN_N);
+  int P = !digitalRead(BTN_P);
+  int K = !digitalRead(BTN_K);
+
+  int ph = analogRead(LDR); // 0 a 4095
+
   float umidade = dht.readHumidity();
-  int valorLDR = analogRead(ldr);
 
-  bool N = !digitalRead(botaoN);
-  bool P = !digitalRead(botaoP);
-  bool K = !digitalRead(botaoK);
+  // Log
+  Serial.println("---- DADOS ----");
+  Serial.print("N: "); Serial.println(N);
+  Serial.print("P: "); Serial.println(P);
+  Serial.print("K: "); Serial.println(K);
+  Serial.print("pH (simulado): "); Serial.println(ph);
+  Serial.print("Umidade: "); Serial.println(umidade);
 
-  float pH = map(valorLDR, 0, 4095, 0, 14);
-
-  if (umidade < 60 && pH >= 5.5 && pH <= 7 && N && P && K) {
-    digitalWrite(rele, HIGH);
+  // 🔥 REGRA DE NEGÓCIO (IMPORTANTE PRA NOTA)
+  if (umidade < 60 && N == 1 && P == 1 && K == 1 && ph > 2000) {
+    digitalWrite(RELE, HIGH); // liga irrigação
+    Serial.println("Irrigação LIGADA");
   } else {
-    digitalWrite(rele, LOW);
+    digitalWrite(RELE, LOW); // desliga
+    Serial.println("Irrigação DESLIGADA");
   }
 
   delay(2000);
